f3fd16c6b29d71fbcb52149a9d7b8626857066e2
FRANÇOISE Anthonia <hei.anthonia@gmail.com>
2026-03-19 19:35:17 +0300

    feat: front design courses

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html","./src/**/*.{html,js}","./*.js","./testimonials/testimonials.{html,js}"],
+  content: ["./index.html", "./src/**/*.{html,js}", "./*.js", "./testimonials/testimonials.{html,js}", "./courses/courses.{html,js}"],
   theme: {
     extend: {},
 
